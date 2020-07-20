34ec5551e40689114f4561bb1810d1b7a3090126
Cristian D Biancotti <cristian@mvzn.me>
2020-07-20 02:34:59 -0300

    Visual changes, some material-ui components added and database firestore conected

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,79 @@ import React, { useState, useEffect } from 'react';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
+import db from './firebase';
+import firebase from 'firebase';
+
+export type MsgObject = {
+  username: string;
+  text: string;
+  timestamp: Date;
+};
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState(['']);
-  const [userName, setUserName] = useState('');
+  const [messages, setMessages] = useState<MsgObject[]>([]);
+  const [username, setUsername] = useState('');
 
   useEffect(() => {
-    // effect
-    // return () => {
-    setUserName(prompt('Select a name to use inside this chat:') || '');
-    // console.log(input);
-    // console.log(messages);
-    // cleanup
-    // };
+    db.collection('messages')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => {
+            const msgObject: MsgObject = {
+              username: doc.data().username,
+              text: doc.data().text,
+              timestamp: doc.data().timestamp,
+            };
+            return msgObject;
+          })
+        );
+      });
+  }, []);
+
+  useEffect(() => {
+    setUsername(prompt('Select a name to use inside this chat:') || '');
+    // TODO: Need some changes to add this msg
+    // setMessages([{ username: 'FMClone', text: 'Hi! Welcome!' }]);
     // When [] is empty the code at useEffect runs ONCE when the app loads
   }, []);
 
+  console.log(messages);
   const sendMsg = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setMessages([...messages, userName + ': ' + input]);
+    db.collection('messages').add({
+      text: input,
+      username: username,
+      // this code give you the timestamp at the server time ( prevent problems with the difference at latitud or longitud of each user )
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    // setMessages([...messages, { username, text: input }]);
     setInput('');
   };
 
   return (
     <div className='App'>
       <h1>Hi Bra!</h1>
+      {/* messages themself */}
+      <div className='message-box'>
+        {messages.map((message) => (
+          <>
+            <Message username={username} message={message} />
+          </>
+        ))}
+      </div>
       <form>
         {/* input field */}
         <FormControl>
           <InputLabel>Write a message...</InputLabel>
           <Input value={input} onChange={(e) => setInput(e.target.value)} />
         </FormControl>
-        {/* <input value={input} onChange={(e) => setInput(e.target.value)} /> */}
         {/* button */}
         <Button type='submit' disabled={!input} variant='contained' color='primary' onClick={sendMsg}>
           Send
         </Button>
       </form>
-      {/* messages themself */}
-      {messages.map((message) => (
-        <>
-          <Message text={message} />
-          {/* <p key={message}>{message}</p> */}
-        </>
-      ))}
     </div>
   );
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import './Message.css';
+import { MsgObject } from './App';
 
 type Props = {
-  text: string;
+  message: MsgObject;
+  username: string;
 };
 
-const Message: React.FC<Props> = ({ text }) => {
+const Message: React.FC<Props> = ({ message, username }) => {
+  const isUser = username === message.username;
   return (
-    <div>
-      <h2>{text}</h2>
-    </div>
+    // TODO: Learn BEM
+    <Card className={`message-card ${isUser && 'user-message'}`}>
+      <CardContent>
+        <Typography variant='h5' component='h2' color='textSecondary'>
+          {message.username}: {message.text}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
